86a7e4696c39dd1a3239009c96abaeb0fec81d99
jinalbangur16 <47166007+jinalbangur16@users.noreply.github.com>
2019-03-06 21:23:02 -0500

    Update testfileforAPI.py

--- testfileforAPI.py
+++ testfileforAPI.py
@@ -1,13 +1,32 @@
 import unittest
 import json
 import re
-from github_api import Git_Hub
+from Mocking_Assignment5a_567A import Git_Hub
+
+
+class JSONReader:
+
+    def __init__(self, test_data_path):
+        self.__test_data_path = test_data_path
+
+    def file_json_reader(self, path):
+        if re.compile("users/[^/]+/repos").match(path):
+            path = f"{self.__test_data_path}/repositories.json"
+        elif re.compile("repos/[^/]+/567B/commit").match(path):
+            path = f"{self.__test_data_path}/commit-567B.json"
+
+        with open(path) as file:
+            return json.load(file)
+
 
 class TestGitHubAPI(unittest.TestCase):
     """ Test cases for GitHub API """
 
     def test_repository_commit_summary(self):
         github = Git_Hub("jinalbangur16")
+
+        github._GitHub__reader = JSONReader(
+            "test_data/with-repositories").file_json_reader
 
         self.assertEqual(github.summary(), [
             'Repo: 567B Number of commits: 7'
@@ -16,6 +35,9 @@ class TestGitHubAPI(unittest.TestCase):
     def test_no_commit_summary(self):
         github = Git_Hub("jinalbangur16")
 
+        github._GitHub__reader = JSONReader(
+            "test_data/withno-commits").file_json_reader
+
         self.assertEqual(github.summary(), [
             'Repo: 567B Number of commits: 7'
         ])
@@ -23,7 +45,11 @@ class TestGitHubAPI(unittest.TestCase):
     def test_no_repositories_summary(self):
         github = Git_Hub("jinalbangur16")
 
+        github._GitHub__reader = JSONReader(
+            "test_data/withno-repositories").file_json_reader
+
         self.assertEqual(github.summary(), [])
+
 
 if __name__ == "__main__":
     print("Running unit tests")
